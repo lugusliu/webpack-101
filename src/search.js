2a2a9f3136986c8c0b4f506b2ffbe35f1d77a547
lugusliu <lugusliu@qq.com>
2019-09-09 23:13:55 +0800

    add config to parse css and less

--- src/search.js
+++ src/search.js
@@ -3,9 +3,11 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 
+import "./search.less";
+
 class Search extends Component {
   render() {
-    return <div>Search Text</div>;
+    return <div className="search-text">Search Text</div>;
   }
 }
 
