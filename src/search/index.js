676633988884cb1a27ea4736cefb369c8ae53942
lugusliu <lugusliu@qq.com>
2020-07-05 22:57:33 +0800

    feat: 测试tree shaking

--- src/search/index.js
+++ src/search/index.js
@@ -6,14 +6,15 @@ import ReactDOM from "react-dom";
 import logo from "../assets/img/github.png";
 import "./index.less";
 import { common } from "../../common";
-
+import { a } from "./tree-shaking";
 console.log(common());
 
 class Search extends Component {
   render() {
+    const funcA = a();
     return (
       <div className="search-text">
-        搜d索ss
+        搜d索ss{funcA}
         <img src={logo} alt="logo" />
         Search Text
         <div className="test">测试数据</div>
