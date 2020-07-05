44a91ab41bc74da8c879a146e7e9c2eab6b1a51a
lugusliu <lugusliu@qq.com>
2020-07-05 22:11:28 +0800

    feat: HtmlWebpackExternalsPlugin插件实践

--- webpack.prod.js
+++ webpack.prod.js
@@ -6,6 +6,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackExternalsPlugin = require("html-webpack-externals-plugin");
 
 const setMPA = () => {
   const entry = {};
@@ -110,6 +111,20 @@ module.exports = {
       cssProcessor: require("cssnano")
     }),
     ...htmlWebpackPlugins,
-    new CleanWebpackPlugin()
+    new CleanWebpackPlugin(),
+    new HtmlWebpackExternalsPlugin({
+      externals: [
+        {
+          module: "react",
+          entry: "https://11.url.cn/now/lib/16.2.0/react.min.js",
+          global: "React"
+        },
+        {
+          module: "react-dom",
+          entry: "https://11.url.cn/now/lib/16.2.0/react-dom.min.js",
+          global: "ReactDOM"
+        }
+      ]
+    })
   ]
 };
