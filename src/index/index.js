dda2225ce9297a4081e935163839ef9e211c6575
lugusliu <lugusliu@qq.com>
2020-07-06 23:07:44 +0800

    feat: 大整数打包项目

--- src/index/index.js
+++
@@ -1,5 +0,0 @@
-import "./index.less";
-import { helloworld } from "./helloworld";
-import { common } from "../../common";
-console.log(common());
-document.write(helloworld());
--- src/search/text.js
+++
@@ -1,3 +0,0 @@
-import React from "react";
-
-export default () => <div style={{ color: "green" }}>动态 import</div>;
